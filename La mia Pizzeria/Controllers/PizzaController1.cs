00a266c7d26ed295d953ee471f8e9fde8babcb6d
Gennaro <gennarinodi02@gmail.com>
2022-05-10 11:51:52 +0200

    Add project files.

---
+++ La mia Pizzeria/Controllers/PizzaController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace La_mia_Pizzeria.Controllers
+{
+    public class PizzaController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ La mia Pizzeria/Models/Pizza.cs
@@ -0,0 +1,20 @@
+﻿namespace La_mia_Pizzeria.Models
+{
+    public class Pizza
+    {
+
+        public string Img { get; set; }
+        public string Titolo { get; set; }
+        public string Ingedienti { get; set; }
+        public float Prezzo { get; set; }
+
+    }
+    public Pizza()
+    {
+    }
+
+    public class Pizza (string Img,string Titolo, string Ingredienti,float Prezzo)
+        {
+        }
+
+}
